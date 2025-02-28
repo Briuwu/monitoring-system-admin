705c512530d4d4a3c5e9a6a5694afb75e984b070
ZenithSUS <jeranmerino147@gmail.com>
2025-02-28 09:37:35 +0800

    Added Calculate Expiration Date

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,24 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function expirationDate(dateSubmitted: string, foc: string): string {
+  const date = new Date(dateSubmitted);
+  switch (foc) {
+    case "Monthly":
+      date.setMonth(date.getMonth() + 1);
+      break;
+    case "Annual":
+      date.setFullYear(date.getFullYear() + 1);
+      break;
+    case "Semi Annual":
+      date.setMonth(date.getMonth() + 6);
+      break;
+    case "Quarterly":
+      date.setMonth(date.getMonth() + 3);
+      break;
+    default:
+      throw new Error("Invalid frequency of change");
+  }
+  return date.toISOString().split('T')[0];
+}
