f602a84a4d76a5e031f17390b3b2108506188258
Briuwu <millontebrian@gmail.com>
2025-02-28 08:48:34 +0800

    Add Pending Subscriptions item to dashboard; adjust grid layout for improved display

--- src/pages/dashboard/dashboard-home.tsx
+++ src/pages/dashboard/dashboard-home.tsx
@@ -2,6 +2,7 @@ import { BarChartOverview } from "./components/bar-chart-overview";
 import DueSoon from "./components/due-soon";
 import { Button } from "@/components/ui/button";
 import { Subscriptions } from "./components/subscriptions";
+// import { useRequirement } from "@/hooks/use-requirement";
 
 const items = [
   {
@@ -22,16 +23,51 @@ const items = [
     value: 2,
     color: "bg-red-500",
   },
+  {
+    id: 4,
+    title: "Pending Subscriptions",
+    value: 0,
+    color: "bg-yellow-500",
+  },
 ];
 
 function DashboardHome() {
+  // const { requirementList } = useRequirement();
+
+  // const subscriptionCounts = {
+  //   active: requirementList.filter((item) => item.status.toLowerCase() === "active"),
+  //   inactive: requirementList.filter((item) => item.status.toLowerCase() === "inactive"),
+  //   pending: requirementList.filter((item) => item.status.toLowerCase() === "pending"),
+  //   total: requirementList.length,
+  // };
+  // <Subscriptions
+  //         title="Total Subscriptions"
+  //         count={subscriptionCounts.total}
+  //         color="blue"
+  //       />
+  //       <Subscriptions
+  //         title="Active Subscriptions"
+  //         count={subscriptionCounts.active.length}
+  //         color="green"
+  //       />
+  //       <Subscriptions
+  //         title="Inactive Subscriptions"
+  //         count={subscriptionCounts.inactive.length}
+  //         color="red"
+  //       />
+  //       <Subscriptions
+  //         title="Pending Subscriptions"
+  //         count={subscriptionCounts.pending.length}
+  //         color="yellow"
+  //       />
+
   return (
     <div className="w-full space-y-5">
       <div className="flex items-center justify-between">
         <h2 className="text-xl font-bold">Dashboard Overview</h2>
         <Button>Generate Reports</Button>
       </div>
-      <div className="grid lg:grid-cols-3 gap-2 w-full">
+      <div className="grid lg:grid-cols-4 gap-2 w-full">
         {items.map((item) => (
           <Subscriptions key={item.id} item={item} />
         ))}
