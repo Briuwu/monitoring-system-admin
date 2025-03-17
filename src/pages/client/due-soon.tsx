183287380d4722d22d0e701b0340bd18c7cb283f
Briuwu <millontebrian@gmail.com>
2025-03-17 09:41:45 +0800

    Add filters for compliance requirements based on frequency and expiration dates

--- src/pages/client/due-soon.tsx
+++ src/pages/client/due-soon.tsx
@@ -25,6 +25,41 @@ function DueSoon() {
       getRemainingDays(item.expiration) < 90
   );
 
+  const twoYearsDueSoon = requirements.filter(
+    (item) =>
+      item.frequencyOfCompliance.toLowerCase() === "2 years" &&
+      getRemainingDays(item.expiration) > 0 &&
+      getRemainingDays(item.expiration) < 90
+  );
+
+  const threeYearsDueSoon = requirements.filter(
+    (item) =>
+      item.frequencyOfCompliance.toLowerCase() === "3 years" &&
+      getRemainingDays(item.expiration) > 0 &&
+      getRemainingDays(item.expiration) < 90
+  );
+
+  const fourYearsDueSoon = requirements.filter(
+    (item) =>
+      item.frequencyOfCompliance.toLowerCase() === "4 years" &&
+      getRemainingDays(item.expiration) > 0 &&
+      getRemainingDays(item.expiration) < 90
+  );
+
+  const fiveYearsDueSoon = requirements.filter(
+    (item) =>
+      item.frequencyOfCompliance.toLowerCase() === "5 years" &&
+      getRemainingDays(item.expiration) > 0 &&
+      getRemainingDays(item.expiration) < 90
+  );
+
+  const tenYearsDueSoon = requirements.filter(
+    (item) =>
+      item.frequencyOfCompliance.toLowerCase() === "10 years" &&
+      getRemainingDays(item.expiration) > 0 &&
+      getRemainingDays(item.expiration) < 180
+  );
+
   const semiAnnualDueSoon = requirements.filter(
     (item) =>
       item.frequencyOfCompliance.toLowerCase() === "semi annual" &&
@@ -51,6 +86,11 @@ function DueSoon() {
     ...semiAnnualDueSoon,
     ...quarterlyDueSoon,
     ...monthlyDueSoon,
+    ...twoYearsDueSoon,
+    ...threeYearsDueSoon,
+    ...fourYearsDueSoon,
+    ...fiveYearsDueSoon,
+    ...tenYearsDueSoon,
   ].sort((a, b) => {
     return getRemainingDays(a.expiration) - getRemainingDays(b.expiration);
   });
