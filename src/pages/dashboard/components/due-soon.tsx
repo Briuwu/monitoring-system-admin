3ab2af406856840606ac2f5eed10507707372759
Briuwu <millontebrian@gmail.com>
2025-03-18 13:49:17 +0800

    Update link paths in Due Soon component to reflect new dashboard structure

--- src/pages/dashboard/components/due-soon.tsx
+++ src/pages/dashboard/components/due-soon.tsx
@@ -60,7 +60,7 @@ function DueSoon() {
         <CardContent className="divide-y-2 overflow-y-auto sm:max-h-[325px]">
           {processData.map((item) => (
             <Link
-              to={`/client/requirements/${item.$id}`}
+              to={`/dashboard/requirements/${item.$id}`}
               key={item.$id}
               className="flex items-center justify-between py-4 px-2 hover:bg-neutral-100"
             >
